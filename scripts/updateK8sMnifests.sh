84442ca9b581c8cadb0da1044b0cc618edf327e0
Ubuntu <azureuser@myagent.swzjiti0pkcubh4midu4hqpnzb.bx.internal.cloudapp.net>
2024-12-16 07:23:03 +0000

    update script

--- scripts/updateK8sMnifests.sh
+++ scripts/updateK8sMnifests.sh
@@ -3,7 +3,7 @@ set -x
 REPO_URL="https://BAFGCFPIpQJm3SSCEPbBJUgLlmmdPmbNituL15uHxscteS8r2AyMJQQJ99ALACAAAAAAAAAAAAASAZDOJ9wU@dev.azure.com/sparshwadhwadevops/LearningAzureDevops/_git/LearningAzureDevops"
 git clone "$REPO_URL" /tmp/temp_repo
 cd /tmp/temp_repo
-sed -i "s|image:.*|image: acr1216/$2:$3|g" k8s-specifications/$1-deployment.yaml
+sed -i "s|image:.*|image: acr1216.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
 git add .
 git commit -m "update K8s Manifests"
 git push
