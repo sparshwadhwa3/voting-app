e65b2d0cd2c31aa919c308a0747dab7b47889f24
Sparsh Wadhwa <sparshwadhwadevops@gmail.com>
2024-12-19 19:36:29 +0000

    Updated updateK8sMnifests.sh

--- scripts/updateK8sMnifests.sh
+++ scripts/updateK8sMnifests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-REPO_URL="https://BAFGCFPIpQJm3SSCEPbBJUgLlmmdPmbNituL15uHxscteS8r2AyMJQQJ99ALACAAAAAAAAAAAAASAZDOJ9wU@dev.azure.com/sparshwadhwadevops/LearningAzureDevops/_git/LearningAzureDevops"
+REPO_URL="https://Bvyn3laGEmdLhyy3hK2UCBMf7b1GjIuGhbB0PKTUrSUKV7yBXASRJQQJ99ALACAAAAAAAAAAAAASAZDOyjNh@dev.azure.com/sparshwadhwadevops/LearningAzureDevops/_git/LearningAzureDevops"
 git clone "$REPO_URL" /tmp/temp_repo
 cd /tmp/temp_repo
 sed -i "s|image:.*|image: acr1216.azurecr.io/$2:$3|g" k8s-specifications/$1-deployment.yaml
